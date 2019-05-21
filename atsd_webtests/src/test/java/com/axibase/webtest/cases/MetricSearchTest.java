1d0be7d6d3e8338439b06334d79c83d9d252153a
katrineTY <katrine192002@gmail.com>
2019-05-21 11:11:50 +0300

    change the way of getting the expected count of metrics

--- atsd_webtests/src/test/java/com/axibase/webtest/cases/MetricSearchTest.java
+++ atsd_webtests/src/test/java/com/axibase/webtest/cases/MetricSearchTest.java
@@ -10,7 +10,9 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
@@ -20,8 +22,8 @@ public class MetricSearchTest extends AtsdTest {
     private MetricsForEntityPage metricsForEntityPage;
 
     @DataProvider(name = "filters")
-    public static Object[][] data() {
-        return new Object[][]{{"j*_*", 18}, {"uy", 0}, {"asd", 0}, {"j?m_*", 15}, {"j?", 0}, {"en?ity*", 1}};
+    public static Object[] data() {
+        return new Object[]{"j*_*", "uy", "asd", "j?m_*", "j?", "en?ity*"};
     }
 
     @BeforeMethod
@@ -34,15 +36,22 @@ public class MetricSearchTest extends AtsdTest {
     }
 
     @Test(dataProvider = "filters")
-    public void testWildcardSearch(String filter, int countOfRecords) {
+    public void testWildcardSearch(String filter) {
         ElementsCollection metrics = metricsForEntityPage.setQuerySearch(filter)
                 .search()
                 .getMetrics();
-        assertEquals("Wrong count of records with filter: " + filter, countOfRecords, metrics.size());
+        assertEquals("Wrong count of records with filter: " + filter, getFilteredMetrics(filter).size(), metrics.size());
         for (SelenideElement metric : metrics) {
             assertTrue(String.format("Wrong metric name: filter: '%s' metric name: '%s'", filter, metric.text()),
                     metric.text().matches(filter.replace("*", ".*").replace("?", ".")));
         }
+    }
+
+    private List<SelenideElement> getFilteredMetrics(String filter) {
+        return metricsForEntityPage.getMetrics().stream()
+                .filter(e -> e.text().replace("<td>", "").replace("</td>", "")
+                        .matches(filter.replace("*", ".*").replace("?", ".")))
+                .collect(Collectors.toList());
     }
 
 }
