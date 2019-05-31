46b07f647289702754f0a14546da5682216af912
katrineTY <katrine192002@gmail.com>
2019-05-31 12:54:40 +0300

    change wait method

--- atsd_webtests/src/test/java/com/axibase/webtest/service/CreateAdminAccountTest.java
+++ atsd_webtests/src/test/java/com/axibase/webtest/service/CreateAdminAccountTest.java
@@ -1,16 +1,13 @@
 package com.axibase.webtest.service;
 
 
+import com.axibase.webtest.CommonAssertions;
 import lombok.extern.slf4j.Slf4j;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 import static com.axibase.webtest.PageUtils.urlPath;
 import static com.axibase.webtest.service.AccountService.CREATE_ACCOUNT_TITLE;
-import static com.codeborne.selenide.Selenide.Wait;
 import static com.codeborne.selenide.Selenide.title;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
@@ -29,11 +26,12 @@ public class CreateAdminAccountTest extends AtsdTest {
     public void createAdminUser() {
         if (!LoginService.TITLE.equals(title())) {
             log.info("Trying to create admin...");
-            Wait().withTimeout(Duration.ofSeconds(2)).until(ExpectedConditions.titleIs(CREATE_ACCOUNT_TITLE));
+            CommonAssertions.assertPageTitleAfterLoadEquals(CREATE_ACCOUNT_TITLE);
             assertTrue(generateAssertMessage("Can't create account"), accountService.createAdmin());
             assertEquals(generateAssertMessage("Should get redirect on home page"), "/", urlPath());
         } else {
             log.info("User already created");
         }
     }
+
 }
